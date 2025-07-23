7c155efb8d48ab0846157e11010304a90cbd080b
Sato <sato1214@example.com>
2025-07-23 19:26:27 +0900

    1

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,4 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-config.secret_key_base = ENV["SECRET_KEY_BASE"]
 puts "SECRET_KEY_BASE is: #{ENV['SECRET_KEY_BASE']}"
